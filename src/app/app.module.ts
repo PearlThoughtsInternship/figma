7429db303b552d336d4a54d1b182914d32e829d5
Lyan Dwi Pangestu <gloriancrack@gmai.com>
2021-09-07 06:46:53 +0700

    feat: article and aside

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { NewsItemComponent } from './news-item/news-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    NewsItemComponent
   ],
   imports: [
     BrowserModule,
