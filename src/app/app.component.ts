98c211cd456f7402a28e7f8788f850d989ce4d30
poonkodi <poonkodi@pearlthoughts.com>
2021-09-21 20:29:53 +0530

    add fetured.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,76 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'figma';
+  featured = [
+    {
+      title:
+        'Lessons From Franchise Executives: Jackie Bondanza of Hounds Town USA',
+      description:
+        'Lessons From Franchise Executives: Jackie Bondanza of Hounds Town USA',
+      author: 'Luca Piacentini',
+      media: 'https://img.youtube.com/vi/i-e7w3uZk-4/maxresdefault.jpg',
+    },
+    {
+      title:
+        'Lessons From Franchise Executives: Jackie Bondanza of Hounds Town USA',
+      description:
+        'Lessons From Franchise Executives: Jackie Bondanza of Hounds Town USA',
+      author: 'Luca Piacentini',
+      media: 'https://img.youtube.com/vi/i-e7w3uZk-4/maxresdefault.jpg',
+    },
+    {
+      title:
+        'Lessons From Franchise Executives: Jackie Bondanza of Hounds Town USA',
+      description:
+        'Lessons From Franchise Executives: Jackie Bondanza of Hounds Town USA',
+      author: 'Luca Piacentini',
+      media: 'https://img.youtube.com/vi/i-e7w3uZk-4/maxresdefault.jpg',
+    },
+    {
+      title:
+        'Lessons From Franchise Executives: Jackie Bondanza of Hounds Town USA',
+      description:
+        'Lessons From Franchise Executives: Jackie Bondanza of Hounds Town USA',
+      author: 'Luca Piacentini',
+      media: 'https://img.youtube.com/vi/i-e7w3uZk-4/maxresdefault.jpg',
+    },
+  ];
+  brandNews = [
+    {
+      title:
+        'Lessons From Franchise Executives: Jackie Bondanza of Hounds Town USA',
+      description:
+        'Lessons From Franchise Executives: Jackie Bondanza of Hounds Town USA',
+      author: 'Luca Piacentini',
+      media: 'https://img.youtube.com/vi/i-e7w3uZk-4/maxresdefault.jpg',
+    },
+    {
+      title:
+        'Lessons From Franchise Executives: Jackie Bondanza of Hounds Town USA',
+      description:
+        'Lessons From Franchise Executives: Jackie Bondanza of Hounds Town USA',
+      author: 'Luca Piacentini',
+      media: 'https://img.youtube.com/vi/i-e7w3uZk-4/maxresdefault.jpg',
+    },
+    {
+      title:
+        'Lessons From Franchise Executives: Jackie Bondanza of Hounds Town USA',
+      description:
+        'Lessons From Franchise Executives: Jackie Bondanza of Hounds Town USA',
+      author: 'Luca Piacentini',
+      media: 'https://img.youtube.com/vi/i-e7w3uZk-4/maxresdefault.jpg',
+    },
+    {
+      title:
+        'Lessons From Franchise Executives: Jackie Bondanza of Hounds Town USA',
+      description:
+        'Lessons From Franchise Executives: Jackie Bondanza of Hounds Town USA',
+      author: 'Luca Piacentini',
+      media: 'https://img.youtube.com/vi/i-e7w3uZk-4/maxresdefault.jpg',
+    },
+  ];
 }
